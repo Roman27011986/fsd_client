17f817bb5f0d24d1cbceedb29fe9a964906d21e1
roman.nesterenko@siz.bz <didorsn@gmail.com>
2024-03-25 13:28:57 +0300

    hot fix

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,7 +6,7 @@ import { IBuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: IBuildOptions):
     webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -18,11 +18,13 @@ export function buildPlugins({ paths, isDev }: IBuildOptions):
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin(
-            {
-                openAnalyzer: false,
-            },
-        ),
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+    return plugins;
 }
